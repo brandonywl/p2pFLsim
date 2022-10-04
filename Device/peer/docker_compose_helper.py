7091e0298674de93ee3c346af1b40c50992a0a74
Brandon Yeow <brandonywl@gmail.com>
2022-10-04 14:42:24 +0800

    Update helper to mount test set to device

--- Device/peer/docker_compose_helper.py
+++ Device/peer/docker_compose_helper.py
@@ -1,7 +1,7 @@
 
-total_num_of_peers = 6
+total_num_of_peers = 10
 
-output_file = open("../docker-compose.yml", "w")
+output_file = open("../docker-compose-testing.yml", "w")
 output_file.write("version: '2'\n")
 output_file.write("services:\n")
 for i in range(1, total_num_of_peers+1):
@@ -13,4 +13,7 @@ for i in range(1, total_num_of_peers+1):
     output_file.write("            - PEERS=[]\n")
     output_file.write("        volumes:\n")
     output_file.write("            - ./all_data/saved_data_client_"+str(i)+":/usr/thisdocker/dataset:rw\n")
+    output_file.write("            - ./all_data/saved_data_test:/usr/thisdocker/testset:ro\n")
+    output_file.write("        ports:\n")
+    output_file.write("            - \"5555\"\n")
 output_file.close()
